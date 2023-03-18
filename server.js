f632c2d2a1cee54aaad4bf392fb015f5bd709616
Faz131 <fazms@icloud.com>
2023-03-18 13:46:36 -0700

    fixed notes.html

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) =>
 );
 
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/db/notes.json'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // GET request for reviews
